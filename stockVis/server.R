88b7691fa95ac9421d6ce66824769fbabc43e03f
Daniel Chen <Maveri2k@yahoo.com>
2014-09-17 23:51:59 -0400

    reactive expressions with stockVis

--- stockVis/server.R
+++ stockVis/server.R
@@ -5,14 +5,27 @@ source("helpers.R")
 
 shinyServer(function(input, output) {
 
-  output$plot <- renderPlot({
-    data <- getSymbols(input$symb, src = "yahoo", 
-      from = input$dates[1],
-      to = input$dates[2],
-      auto.assign = FALSE)
-                 
-    chartSeries(data, theme = chartTheme("white"), 
-      type = "line", log.scale = input$log, TA = NULL)
+  dataInput <- reactive({
+    getSymbols(input$symb, src = "yahoo",
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
   })
-  
+
+  finalInput <- reactive({
+    if(!input$adjust) {
+      return(dataInput())
+    } else {
+      adjust(dataInput())
+    }
+  })
+
+  output$plot <- renderPlot({
+    data <- dataInput()
+
+    chartSeries(finalInput(), theme = chartTheme("white"),
+               type = "line", log.scale = input$log,
+               TA = NULL)
+  })
+
 })
